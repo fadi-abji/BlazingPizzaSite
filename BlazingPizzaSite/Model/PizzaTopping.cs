2572df65718da819500e57fd96ccafeb4cce700b
Fadi Abji <fadi@wexer.se>
2024-08-01 11:55:52 +0200

    Add project files.

---
+++ BlazingPizzaSite/Model/PizzaTopping.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazingPizzaSite.Model
+{
+    public class PizzaTopping
+    {
+        public Topping Topping { get; set; }
+
+        public int ToppingId { get; set; }
+        
+        public int PizzaId { get; set; }
+    }
+}
---
+++ BlazingPizzaSite/Model/UserInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazingPizzaSite.Model
+{
+    public class UserInfo
+    {
+        public bool IsAuthenticated { get; set; }
+
+        public string Name { get; set; }
+    }
+}
