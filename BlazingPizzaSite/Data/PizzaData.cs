e11d16a770bfda661061d28dec324151951eaa26
Fadi Abji <fadi@wexer.se>
2024-08-05 15:06:34 +0200

    extends data validations in server side

--- BlazingPizzaSite/Data/PizzaData.cs
+++ BlazingPizzaSite/Data/PizzaData.cs
@@ -1,17 +1,21 @@
-﻿namespace BlazingPizzaSite.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazingPizzaSite.Data
 {
     public class PizzaData
     {
-        public int PizzaId { get; set; }
+        public int Id { get; set; }
 
+        [Required(ErrorMessage = "You must set a name for your pizza.")]
         public string Name { get; set; }
 
         public string Description { get; set; }
 
+        [EmailAddress(ErrorMessage = "You must set a valid email address for the chef responsible for the pizza recipe.")]
+        public string ChefEmail { get; set; }
+
+        [Required]
+        [Range(10.00, 25.00, ErrorMessage = "You must set a price between $10 and $25.")]
         public decimal Price { get; set; }
-
-        public bool Vegetarian { get; set; }
-
-        public bool Vegan { get; set; }
     }
 }
